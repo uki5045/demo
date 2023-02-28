cac1a6251414b686e29001e4076cd9922218cb37
hanjinwook <uki5045@gmail.com>
2023-02-28 13:04:08 +0900

    chart_modify

--- src/pages/Chart.js
+++ src/pages/Chart.js
@@ -1,55 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { TickerTape } from "react-ts-tradingview-widgets";
 
 const Chart = () => {
-  const scriptRef = useRef(null);
-
-  useEffect(() => {
-    if (!scriptRef.current) {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src =
-        "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
-      script.async = true;
-      script.innerHTML = `
-      {
-        "symbols": [
-          {
-            "description": "",
-            "proName": "BINANCE:BTCUSDT"
-          },
-          {
-            "description": "",
-            "proName": "BITGET:XRPUSDT.P"
-          },
-          {
-            "description": "",
-            "proName": "SKILLING:NASDAQ"
-          },
-          {
-            "description": "",
-            "proName": "TVC:GOLD"
-          }
-        ],
-        "showSymbolLogo": true,
-        "colorTheme": "light",
-        "isTransparent": false,
-        "displayMode": "adaptive",
-        "locale": "en"
-      }
-      `;
-
-      scriptRef.current = script;
-      const containerRef = document.querySelector(
-        ".tradingview-widget-container"
-      );
-      containerRef.appendChild(script);
-    }
-  }, []);
-
   return (
-    <div className="tradingview-widget-container">
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright"></div>
+    <div>
+      <TickerTape colorTheme="light"></TickerTape>
     </div>
   );
 };
